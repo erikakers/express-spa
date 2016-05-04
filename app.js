99ec82f2b18a2accbdf185bcb976f975db35c1e5
Erik Akers <erikakers@gmail.com>
2016-05-04 11:48:49 -0500

    adding kraken-js and basic production server setup

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
-console.log('production server');
+/* eslint new-cap: [2, {"capIsNewExceptions": ["Server"]}] */
 
+import express from 'express';
+import kraken from 'kraken-js';
+import { join } from 'path';
 import webpack from 'webpack';
 
-// TODO: Test this.
+const app = express();
+const http = require('http').Server(app);
+const port = process.env.PORT || 5000;
 const factory = require('./webpack.factory');
 const compiler = webpack(factory({
     env: process.env.NODE_ENV,
     typescript: true
 }));
 
-compiler.run();
+compiler.run((error, stats) => {
+    if (error) {
+        console.log(error);
+    } else { // eslint-disable-line
+        console.log(stats.toString());
+    }
+});
+
+app.use(kraken());
+
+app.use(express.static( join(__dirname, 'public') ));
+
+app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, 'public', 'index.html'));
+});
+
+http.listen(port, (error) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+
+    console.log(`${process.env.NODE_ENV} server running on port: ${port}`);
+});
